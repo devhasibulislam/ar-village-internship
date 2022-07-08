0d50b9447aab73d2bb735732b51bfe36aab35ef0
hasibulislam999 <hasib143sl@gmail.com>
2022-07-08 23:03:22 +0600

    canvas performence included along side with yt video

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import CanvasDraw from "react-canvas-draw";
 
 function App() {
   const [URL, setURL] = useState('');
@@ -9,8 +10,6 @@ function App() {
 
     const url = event.target.yt_url.value;
     setURL(url.replace('watch?v=', 'embed/'));
-
-    // https://www.youtube.com/watch?v=UPNkOwabRDY
 
     event.target.reset();
   };
@@ -58,15 +57,30 @@ function App() {
                 </p>
               </div>
               :
-              <iframe
-                width="100%"
-                height="315"
-                src={URL}
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
+              <div id='canvas-box'>
+                <div>
+                  <iframe
+                    width="529"
+                    height="315"
+                    src={URL}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  />
+                </div>
+                <div>
+                  <CanvasDraw
+                    style={{
+                      boxShadow:
+                        "0 13px 27px -5px rgba(50, 50, 93, 0.25), 0 8px 16px -8px rgba(0, 0, 0, 0.3)"
+                    }}
+                    canvasWidth={529}
+                    canvasHeight={315}
+                    enablePanAndZoom={true}
+                  />
+                </div>
+              </div>
           }
         </div>
       </div>
